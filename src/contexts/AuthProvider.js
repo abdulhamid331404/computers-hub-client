518f1ebb3e237477f1bf0ad8b892e05b192094ab
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-30 05:00:28 +0600

    create Auth Context

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import {getAuth} from 'firebase/auth'
+import app from '../firebase/firebase.config';
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+const authInfo = {
+
+}
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
